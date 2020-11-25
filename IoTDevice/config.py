0cf1c041cb82c2d9e1378238fb6441771301e8a4
Aidan Clyens <aidanc@rogers.com>
2020-11-24 22:48:21 -0500

    Make config file blank initially

--- IoTDevice/config.py
+++ IoTDevice/config.py
@@ -1,7 +1,7 @@
 endpoint = "airgahux2exxu-ats.iot.us-east-2.amazonaws.com"
-client_id = "LaptopTest"
-path_to_cert = "certificates/7b6f6d99ce-certificate.pem.crt"
-path_to_key = "certificates/7b6f6d99ce-private.pem.key"
-path_to_root = "certificates/root.pem"
-topic = "HubTopic"
+client_id = ""
+path_to_cert = ""
+path_to_key = ""
+path_to_root = ""
+topic = ""
 
