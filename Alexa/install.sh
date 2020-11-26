1d20bd2af4f675e83715f440fcdf4c3674248ac5
Aidan Clyens <aidanc@rogers.com>
2020-11-25 22:35:31 -0500

    Update install script

---
+++ Alexa/install.sh
@@ -0,0 +1,21 @@
+mkdir alexa
+cd alexa
+if ! [ -f "setup.sh" ]; then
+    wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/setup.sh 
+fi
+
+if ! [ -f "genConfig.sh" ]; then
+    wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/genConfig.sh
+fi
+
+if ! [ -f "pi.sh" ]; then
+    wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/pi.sh
+fi
+
+if [ -f "../config.json" ]; then
+    bash setup.sh ../config.json
+fi
+
+sudo apt-get install gstreamer1.0-alsa gstreamer1.0-tools gstreamer1.0-plugins-ugly
+
+cd ..
