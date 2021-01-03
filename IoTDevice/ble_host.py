d766b4c4a29b14b35478fa0ec768f8aaed4f9283
Aidan Clyens <aidanc@rogers.com>
2021-01-03 15:37:56 -0500

    Create BLEHost class

--- IoTDevice/ble_host.py
+++ IoTDevice/ble_host.py
@@ -1,31 +1,42 @@
 from gattlib import DiscoveryService, GATTRequester
 
 
-target_address = "0C:61:CF:A3:09:3E"
+device_address = "0C:61:CF:A3:09:3E"
 
 name_uuid = "00002a00-0000-1000-8000-00805f9b34fb"
 
 
-def scan_for_device(address):
-    service = DiscoveryService()
-    devices = service.discover(2)
+class BLEHost:
+    discovery_service = None
+    requester = None
 
-    if not address in devices:
-        print("Device not found")
-        exit()
+    def scan(self, target_address, timeout=2):
+        self.discovery_service = DiscoveryService()
+        self.devices = self.discovery_service.discover(timeout)
 
+        if not target_address in self.devices:
+            print(f"Device {target_address} not found")
+            exit()
 
-def connect(address):
-    print("Found device. Connecting...")
-    requester = GATTRequester(address)
+    def connect(self, target_address):
+        print(f"Found device {target_address}. Connecting...")
+        self.requester = GATTRequester(target_address)
 
-    name = requester.read_by_uuid(name_uuid)[0]
-    print("Connected to: ", name.decode())
+        name = self.get_name()
+        print("Connected to: ", name.decode())
+
+    def get_name(self):
+        if self.requester is None:
+            print("Error. No device is connected")
+            return ""
+        
+        return self.requester.read_by_uuid(name_uuid)[0]
 
 
 def main():
-    scan_for_device(target_address)
-    connect(target_address)
+    ble = BLEHost()
+    ble.scan(device_address)
+    ble.connect(device_address)
 
     while True:
         pass
