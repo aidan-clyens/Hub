c4d2ce5b614fdff1d5ee314a920226e4e1997776
Aidan Clyens <aidanc@rogers.com>
2021-03-27 11:44:15 -0400

    Increase Amazon Polly volume

--- rpihub/alexa/speech.sh
+++ rpihub/alexa/speech.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 say() {
     ./venv/bin/aws polly synthesize-speech --output-format mp3 --voice-id Joanna --text "$*" tmp.mp3;
-    local IFS=+;/usr/bin/mplayer -ao alsa -noconsolecontrols tmp.mp3;
+    local IFS=+;/usr/bin/mplayer -af volume=10:1 -ao alsa -noconsolecontrols tmp.mp3;
     rm tmp.mp3
 }
 say $*
